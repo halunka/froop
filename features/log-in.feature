f5d95437cfa44d04aefd0646953923450abf33dd
Kriegslustig <github@ls7.ch>
2014-12-21 23:02:11 +0100

    Adding scenario Log In

--- features/log-in.feature
+++ features/log-in.feature
@@ -4,8 +4,9 @@ Feature: Log in
   I want to log into the web app
 
   Scenario: Logging in with the right credentials
-    Given I am on the "/home" page
+    Given I am on the "/login" page
     And I am not logged in
     When fill in "E-mail" with the right email address
     And I fill in "Password" with the right password
-    Then I should see "HBFF's Fancy Foodie Blog"
+    And I click on "Log in"
+    Then I should be on "/"
---
+++ features/step_definitions/log-in_steps.rb
@@ -0,0 +1,25 @@
+Given(/^I am on the "(.*?)" page$/) do |page|
+  visit(page)
+  expect(current_path).to eq page
+end
+
+Given(/^I am not logged in$/) do
+  expect(page).to have_content 'Log In'
+end
+
+When(/^fill in "(.*?)" with the right email address$/) do |field|
+  expect(page).to have_field field
+end
+
+When(/^I fill in "(.*?)" with the right password$/) do |field|
+  expect(page).to have_field field
+end
+
+When(/^I click on "(.*?)"$/) do |button|
+  expect(page).to have_button button
+  click_button button
+end
+
+Then(/^I should be on "(.*?)"$/) do |page|
+  expect(current_path).to eq page
+end
