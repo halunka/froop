9b74a98412f0bfae18b2935c288e2e8bc80555b6
mala23 <mail@mala23.me>
2014-12-25 14:50:07 +0100

    toggle nav class

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,3 +1,36 @@
+window.addEventListener('load', function() {
+
+  toggleNav.init();
+
+}, false);
+
+var toggleNav = {};
+toggleNav.state = false;
+toggleNav.toggleClass = 'main-nav--open';
+
+toggleNav.init = function() {
+  this.mainNav = document.querySelector('.main-nav');
+  this.setEventListeners();
+};
+
+toggleNav.setEventListeners = function() {
+  this.mainNav.addEventListener('click', function() {
+    toggleNav.toggle(); 
+  }, false);
+};
+
+toggleNav.toggle = function() {
+  if(!this.state) {
+    this.mainNav.className += ' ' + this.toggleClass;
+  } else {
+    this.mainNav.className = this.mainNav.className.replace(' ' + this.toggleClass, '');
+  };
+  this.state = !this.state;
+
+};
+
+/*
+
 $(".open-panel").click(function(){
     
     $("html").addClass("openNav");
@@ -9,3 +42,5 @@ $(".close-panel, #content").click(function(){
 $("html").removeClass("openNav");
     
 });
+
+*/
